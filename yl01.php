84e4ca0120526a4b6f4fe2250761fa77339f0ce4
Anri Maidla <amaidla@hkhk.edu.ee>
2026-04-07 08:58:51 +0300

    1
    1

---
+++ yl01.php
@@ -0,0 +1,13 @@
+<?php
+
+$nimi = "Anri";
+$kuupaev = 07.04;
+
+echo $nimi . PHP_EOL . PHP_EOL . PHP_EOL;
+
+echo "Väljasta järgnev lause: “Karin Eegreid” – Ulle Doos" . PHP_EOL;
+
+echo "   /\\_/\\   " . PHP_EOL;
+echo "  ( o.o )  " . PHP_EOL;
+echo "   > ^ <   " . PHP_EOL;
+?>
